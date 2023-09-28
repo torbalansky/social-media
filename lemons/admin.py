c8d3fabc584d5af0db1114198fe378b7b571285c
torbalansky <torbalansky@gmail.com>
2023-09-28 18:55:05 +0100

    Added profile_list and profile views

--- lemons/admin.py
+++ lemons/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+#Unregister group
+admin.site.unregister(Group)
+
+#Combine profile with user
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+#expand user model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+#unregister user
+admin.site.unregister(User)
+
+#register user and profile
+admin.site.register(User, UserAdmin)
+#admin.site.register(Profile)
+
--- lemons/views.py
+++ lemons/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Profile
 
 def home(request):
     return render(request, 'home.html', {})
+
+def profile_list(request):
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html', {"profiles":profiles})
+    else:
+        messages.success(request, ("You must be logged in to access this."))
+        return redirect('home')
+
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        return render(request, "profile.html", {"profile":profile})
+    else:
+        messages.success(request, ("You must be logged in."))
+        return redirect('home')
